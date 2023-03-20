3aeca3da5a2fe2fb388f22ee309c286b5c9b0800
Łukasz Kuk <lukasz010420@gmail.com>
2023-03-20 23:56:46 +0100

    use api to present news

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
+import requests
+from send_email import send_email
+
+topic = "tesla"
+api_key = "cd9657a739a043b0a6ba851b0d3431e0"
+url = "https://newsapi.org/v2/everything?" \
+      f"q={topic}" \
+      "&from=2023-02-20&sortBy=publishedAt" \
+      "&apiKey=cd9657a739a043b0a6ba851b0d3431e0" \
+      "&language=en"
+
+
+def main():
+    request = requests.get(url)
+    content = request.json()
+    body = ""
+    for article in content["articles"][:20]:
+        if article["title"] is not None:
+            body = "Subject: Today's news" + body \
+                   + article["title"] + "\n" \
+                   + article["description"] + "\n" \
+                   + article["url"] + 2 * "\n"
+
+    send_email(message=body)
+
 
 if __name__ == "__main__":
-    pass
+    main()
---
+++ send_email.py
@@ -0,0 +1,4 @@
+def send_email(message: str):
+    print(message)
+    message = message.encode("utf-8")
+    pass
